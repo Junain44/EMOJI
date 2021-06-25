62823ca64e0186dc150443afc7227e942ea460b0
Junain44 <junaingallie44@gmail.com>
2021-06-26 00:17:16 +0200

    second fit

---
+++ app.js
@@ -0,0 +1,19 @@
+// Targeting the divs in the html
+const closedFace = document.querySelector('.closed')
+const openFace = document.querySelector('.open')
+
+
+//Adding the function every click will do something.
+closedFace.addEventListener('click', () => {
+    if(openFace.classList.contains('open')){
+       openFace.classList.add('active');
+       closedFace.classList.remove('active');
+    }
+});
+
+openFace.addEventListener('click', () => {
+    if(closedFace.classList.contains('closed')){
+        closedFace.classList.add('active')
+         openFace.classList.remove('active')
+    }
+});
